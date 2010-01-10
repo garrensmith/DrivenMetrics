9b358b5b70450e494eacb338b92a02c5b8678b3e
garren smith <garren.smith@gmail.com>
2010-01-10 19:03:14 +0200

    small unit test changes

--- src/DrivenMetrics.Tests/Core/AssemblyLoaderTests.cs
+++ src/DrivenMetrics.Tests/Core/AssemblyLoaderTests.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+using System.IO;
 using NUnit.Framework;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
 
 namespace DrivenMetrics.Tests
 {
@@ -20,6 +22,29 @@ namespace DrivenMetrics.Tests
             var assemblyLoader = new AssemblyLoader("fake.dll");
             Assert.Throws <FileNotFoundException>(() => assemblyLoader.Load());
         }
+		
+		[Test]
+		public void ShouldLoadSymbols()
+		{
+			var assembly = AssemblyFactory.GetAssembly("DomainTestClasses.dll");
+			
+			assembly.MainModule.LoadSymbols();
+			
+			foreach(TypeDefinition t in assembly.MainModule.Types)
+			{
+				foreach(MethodDefinition m in t.Methods)
+				{
+					foreach(Instruction ins in m.Body.Instructions)
+					{
+						if (ins.SequencePoint != null)
+							System.Console.WriteLine(ins.SequencePoint.ToString());
+					}
+				}
+				
+				System.Console.WriteLine(t.ToString());
+			}
+			
+		}
 
        /* public void ShouldThrowExceptionOnMissingPdb()
         {
