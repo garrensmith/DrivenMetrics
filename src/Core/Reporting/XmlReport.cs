cc2129c5f2e5117e7a2d214eb339078b5005527f
Olivier Dagenais <olivier.dagenais@gmail.com>
2011-02-05 11:17:53 -0500

    Added the skeleton for XmlReport, a new IReport implementation.

---
+++ src/Core/Reporting/XmlReport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Driven.Metrics.Metrics;
+
+namespace Driven.Metrics.Reporting
+{
+    public class XmlReport : IReport
+    {
+        #region IReport Members
+
+        public void Generate (IEnumerable<MetricResult> results)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public void Generate (MetricResult result)
+        {
+            throw new NotImplementedException ();
+        }
+
+        #endregion
+    }
+}
