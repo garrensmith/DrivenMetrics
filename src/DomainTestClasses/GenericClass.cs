458f8101853fbae49d9de1e2144f89d5f4a1475b
Olivier Dagenais <olivier.dagenais@gmail.com>
2011-02-05 15:37:57 -0500

    Added the GenericClass for testing HTML reports and updated two unit tests accordingly.

---
+++ src/DomainTestClasses/GenericClass.cs
@@ -0,0 +1,17 @@
+﻿namespace DomainTestClasses
+{
+    public class GenericClass<T>
+    {
+        private readonly T[] _ts;
+
+        public GenericClass(T[] source)
+        {
+            _ts = source;
+        }
+
+        public override string ToString ()
+        {
+            return _ts.Length.ToString ();
+        }
+    }
+}
