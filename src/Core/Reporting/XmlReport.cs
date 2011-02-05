ff97cd0efa3a2c85d00a8ce4f2f2c4c239854460
Olivier Dagenais <olivier.dagenais@gmail.com>
2011-02-05 11:55:04 -0500

    Added a constructor to XmlReport as well as functionality to output the report to the IFileWriter. Added a very simple test in the new XmlReportTest.

--- src/Core/Reporting/XmlReport.cs
+++ src/Core/Reporting/XmlReport.cs
@@ -1,21 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
 using Driven.Metrics.Metrics;
 
 namespace Driven.Metrics.Reporting
 {
     public class XmlReport : IReport
     {
+        private static readonly XmlWriterSettings XmlWriterSettings = new XmlWriterSettings
+        {
+            NewLineChars = "\n",
+            Indent = true,
+            IndentChars = "  ",
+            OmitXmlDeclaration = true,
+        };
+
+        private readonly IFileWriter _fileWriter;
+        private readonly string _filePath;
+        private readonly XDocument _doc;
+
+        public XmlReport(IFileWriter fileWriter, string filePath)
+        {
+            _fileWriter = fileWriter;
+            _filePath = filePath;
+            _doc = new XDocument(new XElement("metrics"));
+        }
+
+        public override string ToString ()
+        {
+            var sb = new StringBuilder ();
+            using (var writer = XmlWriter.Create (sb, XmlWriterSettings))
+            {
+                Debug.Assert (writer != null);
+                _doc.WriteTo (writer);
+            }
+            return sb.ToString ();
+        }
+
         #region IReport Members
 
         public void Generate (IEnumerable<MetricResult> results)
         {
-            throw new NotImplementedException ();
+            // TODO: convert results to nodes and add them to _doc
+            var contents = ToString ();
+            _fileWriter.Write (_filePath, contents);
         }
 
         public void Generate (MetricResult result)
         {
-            throw new NotImplementedException ();
+            Generate (new [] { result});
         }
 
         #endregion
---
+++ src/DrivenMetrics.Tests/Reporting/XmlReportTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Linq;
+using Driven.Metrics.Reporting;
+using NUnit.Framework;
+
+namespace Driven.Metrics.Tests.Reporting
+{
+    [TestFixture]
+    public class XmlReportTest
+    {
+        private static void AssertAreEqual(string expectedXml, XmlReport actualReport)
+        {
+            var expectedElement = XElement.Parse (expectedXml);
+            Assert.AreEqual (expectedElement.ToString (), actualReport.ToString ());
+        }
+
+        [Test]
+        public void EmptyReport()
+        {
+            var report = new XmlReport (null, null);
+            AssertAreEqual ("<metrics />", report);
+        }
+    }
+}
