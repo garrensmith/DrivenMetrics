79351fc896a164eb8da742936e5b4e8fc5ee3936
GarrenSmith <garren.smith@gmail.com>
2011-02-07 21:41:36 +0200

    bump version to 1.1

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProductAttribute("DrivenMetrics")]
 [assembly: AssemblyCopyrightAttribute("Released under the Apache 2.0 License")]
-[assembly: AssemblyInformationalVersionAttribute("0.1.0.0")]
-[assembly: AssemblyVersionAttribute("0.1.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("0.1.1.0")]
+[assembly: AssemblyVersionAttribute("0.1.1.0")]
 		
